43c643f72d418731cfa77d07bb01481a0a70b48b
Alexander671 <matveevalex97@yandex.ru>
2022-01-27 23:29:01 +0300

    commit - added test_models

---
+++ iot/test_models.py
@@ -0,0 +1,15 @@
+class YourTestClass(TestCase):
+
+    def setUp(self):
+        # Установки запускаются перед каждым тестом
+        pass
+
+    def tearDown(self):
+        # Очистка после каждого метода
+        pass
+
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
+
+    def test_something_that_will_fail(self):
+        self.assertTrue(False)
